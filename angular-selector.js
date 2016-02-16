ff4d9e8d2be71ca0471e9829be1cc06535464811
Pavel Paleček <palecek.pavel@gmail.com>
2016-02-16 20:34:04 +0100

    commonJS module style. Release 1.0.3

--- angular-selector.js
+++ angular-selector.js
@@ -1,9 +1,10 @@
 'use strict';
+require('angular');
 
 // Key codes
 var KEYS = { up: 38, down: 40, left: 37, right: 39, escape: 27, enter: 13, backspace: 8, delete: 46, shift: 16, leftCmd: 91, rightCmd: 93, ctrl: 17, alt: 18, tab: 9 };
 
-var Selector = (function () {
+var Selector = (function ($filter, $timeout, $window, $http, $q) {
 
 	function getStyles(element) {
 		return !(element instanceof HTMLElement) ? {} :
